245ca7c89056d6b2ffef24f9bb003d436f95a7b2
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2023-02-27 21:50:40 +0100

    ex 5 finished

---
+++ exercises/week1/ex5.py
@@ -0,0 +1,13 @@
+# Scrivere un programma per controllare se tutti i caratteri
+# di s1 sono contenuti in s2.
+# La posizione del carattere non ha importanza.
+# Ad esempio con s1='by' e s2='blueberry' dovrebbe restituire true.
+# Al contrario, con s1='byZ' e s2='blueberry' dovrebbe restituire false.
+
+import functools
+
+s1="by"
+s2="bluebarry"
+
+print(functools.reduce(lambda b1,b2: b1 and b2,[c in s2 for c in s1]))
+
