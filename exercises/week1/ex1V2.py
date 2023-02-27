4a2619b3c326112e75166d06066d55eda9fce8e5
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2023-02-27 21:27:21 +0100

    Refactor ex1v2.py

--- exercises/week1/ex1V2.py
+++ exercises/week1/ex1V2.py
@@ -1,15 +1,2 @@
 # Print all elements of a given list that are less then 5.
-
-def less_then_five(l:range) -> list:
-    """Return a list of elements less then 5.
-
-    Args:
-        l (list): The list of numbers.
-
-    Returns:
-        list: The list of elements less then 5.
-    """
-    return [num for num in l if num < 5]
-
-
-print(less_then_five(list(range(1, 10))))
+print([num for num in range(1, 10) if num < 5])
