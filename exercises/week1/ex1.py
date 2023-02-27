0b6de46b7680ed1f52ee93254b7b2bcc899c42f6
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2023-02-27 21:25:23 +0100

    Refactor ex1

--- exercises/week1/ex1.py
+++ exercises/week1/ex1.py
@@ -1,14 +1,2 @@
 # Print all elements of a given list that are less then 5.
-def filtered_list(l:range, f:filter) -> list:
-    """A filtered list using f filter.
-
-    Args:
-        l (list): The list to be filtered.
-        f (filter): The filter function.
-
-    Returns:
-        list: The list filtered.
-    """
-    return list(filter(f, l))
-
-print(filtered_list(list(range(1,10)), lambda x: x<5))    
+print(list(filter(lambda x: x<5, range(1,10))))
