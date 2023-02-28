d56d7a59df9825cb044501026e7d69219974c800
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2023-02-28 10:34:50 +0100

    Fix indexes

--- exercises/week1/ex4.py
+++ exercises/week1/ex4.py
@@ -1,3 +1,3 @@
 # Scrivete un programma per stampare il seguente modello usando i cicli for.
 MAX=6
-[print('*'*i) for i in list((range(1,MAX))) + list(range(MAX,0,-1))]
+[print('*'*i) for i in list((range(1,MAX))) + list(range(MAX-2,0,-1))]
