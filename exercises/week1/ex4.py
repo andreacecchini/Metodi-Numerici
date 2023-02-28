48274c2d4ac98ca7677fb5c667bbec2d309dbb6a
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2023-02-28 10:33:05 +0100

    Refactor ex4

--- exercises/week1/ex4.py
+++ exercises/week1/ex4.py
@@ -1,4 +1,3 @@
 # Scrivete un programma per stampare il seguente modello usando i cicli for.
 MAX=6
-[print('*'*i) for i in range(1,MAX)]
-[print('*'*(MAX-i)) for i in range(2,MAX)]
+[print('*'*i) for i in list((range(1,MAX))) + list(range(MAX,0,-1))]
